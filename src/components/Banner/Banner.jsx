730b93fcad5d5cc8271d4c9bb41d55dac03a0f18
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-01-14 16:52:10 +0530

    style updated for banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,24 +1,40 @@
 import React from "react";
-
 import "./Banner.css";
 import Arrow from "../../assets/Arrow.jsx";
+
 function Banner() {
   return (
     <div className="bannerParentDiv">
       <div className="bannerChildDiv">
         <div className="menuBar">
           <div className="categoryMenu">
-            <span>ALL CATEGORIES</span>
+            <a href="#" className="category-link">
+              ALL CATEGORIES
+            </a>
             <Arrow></Arrow>
           </div>
           <div className="otherQuickOptions">
-            <span>Cars</span>
-            <span>Motorcycles</span>
-            <span>Mobile Phones</span>
-            <span>For Sale: Houses & Apartments</span>
-            <span>Scooters</span>
-            <span>Commercial Vehicles</span>
-            <span>For Rent: Houses & Apartments</span>
+            <a href="#" className="category-link">
+              Cars
+            </a>
+            <a href="#" className="category-link">
+              Motorcycles
+            </a>
+            <a href="#" className="category-link">
+              Mobile Phones
+            </a>
+            <a href="#" className="category-link">
+              For Sale: Houses & Apartments
+            </a>
+            <a href="#" className="category-link">
+              Scooters
+            </a>
+            <a href="#" className="category-link">
+              Commercial Vehicles
+            </a>
+            <a href="#" className="category-link">
+              For Rent: Houses & Apartments
+            </a>
           </div>
         </div>
         <div className="banner">
